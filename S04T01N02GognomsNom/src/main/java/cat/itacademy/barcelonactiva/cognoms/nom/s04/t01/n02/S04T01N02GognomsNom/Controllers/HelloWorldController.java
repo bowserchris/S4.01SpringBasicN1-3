17c30adf966a8f0071318f37a4383f2f13f4f281
bowserchris <133642309+bowserchris@users.noreply.github.com>
2023-09-12 17:19:51 +0200

    Add files via upload

---
+++ S04T01N02GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/S04T01N02GognomsNom/Controllers/HelloWorldController.java
@@ -0,0 +1,30 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n02.S04T01N02GognomsNom.Controllers;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/HelloWorld")
+	@ResponseBody
+	public String hello(@RequestParam(defaultValue = "UNKNOWN") String name) {
+		return "Hello " + name + ". You´re executing a Gradle Project.";
+	}
+	
+	@RequestMapping(value = {"/HelloWorld2", "/HelloWorld2/{name}"})
+	public String hello2(@PathVariable Optional<String> name) {
+		if (name.isPresent()) {
+			return "Hello " + name.get() + ". You´re executing a Gradle Project.";
+		} else {
+			return "value is optional and not present.";
+		}
+	}
+
+}
